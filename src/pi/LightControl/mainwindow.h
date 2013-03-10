9fc4cf90550c8863b1c5423c740a2a5c1e6a75b5
Adam Eliot <adameliot@gmail.com>
2013-03-10 17:20:46 -0400

    helps to save the new header

--- src/pi/LightControl/mainwindow.h
+++ src/pi/LightControl/mainwindow.h
@@ -13,10 +13,15 @@ class MainWindow : public QMainWindow
     
 public:
     explicit MainWindow(QWidget *parent = 0);
+    void dial();
     ~MainWindow();
     
+private slots:
+
 private:
     Ui::MainWindow *ui;
+    int key;
+    int keyOld;
 };
 
 #endif // MAINWINDOW_H
